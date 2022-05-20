61ed932aa96de3628246656a2b8dbbee3765a4a3
mhuertas <manuel.huertas.lopez@gmail.com>
2022-05-20 09:54:51 +0000

    Clean code

--- Client.py
+++ Client.py
@@ -14,92 +14,12 @@ _QUERY_MAGNITUDE_TOKEN = "idmagnitud"
 _QUERY_PAGE_START_TOKEN = "iDisplayStart"
 _QUERY_PAGE_LENGTH_TOKEN = "iDisplayLength"
 
-query = {
-    'search':
-        {
-            'date_ini': datetime(2021, 3, 1, 20, 0, 0),
-            'date_end': datetime(2021, 3, 1, 20, 0, 30),
-            'time_ini': time(20, 0, 0),
-            'time_end': time(7, 0, 0),
-            'days': 31
-        },
-
-    'monitors':
-        [
-            {
-                'id': 3623,
-                'name': 'MACS.AzimuthAxis.position',
-                'period': 200000,
-                'epsilon': 0.5
-            },
-            {
-                'id': 3625,
-                'name': 'MACS.AzimuthAxis.followingError',
-                'period': 200000,
-                'epsilon': 0.00002
-            },
-            {
-                'id': 3696,
-                'name': 'MACS.ElevationAxis.position',
-                'period': 200000,
-                'epsilon': 0.5
-            },
-            {
-                'id': 12128,
-                'name': 'ECS.UpperShutter.actualPosition',
-                'period': 1000000,
-                'epsilon': 0.5
-            },
-            {
-                'id': 667,
-                'name': 'ECS.DomeRotation.actualPosition',
-                'period': 1000000,
-                'epsilon': 0.5
-            },
-            {
-                'id': 8264,
-                'name': 'EMCS.WeatherStation.meanWindSpeed',
-                'period': 1000000,
-                'epsilon': 1.0
-            },
-            {
-                'id': 8265,
-                'name': 'EMCS.WeatherStation.windDirection',
-                'period': 1000000,
-                'epsilon': 1.0
-            },
-            {
-                'id': 8116,
-                'name': 'OE.ObservingEngine.slowGuideErrorA',
-                'period': 1000000,
-                'epsilon': 0.01
-            },
-            {
-                'id': 8117,
-                'name': 'OE.ObservingEngine.slowGuideErrorB',
-                'period': 1000000,
-                'epsilon': 0.01
-            }
-        ],
-
-    'magnitudes':
-        [
-            {
-                'id': 4238,
-                'name': 'OE.ObservingEngine.currentObservingState',
-            }
-        ]
-
-}
-
-
 def _parse_raw_test(text):
     text = text.split('\n')
 
     text_header = ','.join(text[0].replace("/", ".").split(",")[1:])
     #todo extract this info to metadata monitor
     text_header = re.sub("\(.*\)", "", text_header)
-
 
     text_body = [','.join(line.split(",")[1:]) for line in text[1:]]
     text_body = '\n'.join(text_body)
@@ -122,11 +42,13 @@ class Cursor(object):
         if self._current >= self._pages:
             raise StopIteration
 
-        final_uri = self.url_base + "/download" + \
+        final_uri = _DEFAULT_BASE_URL + "/download" + self.url_base +  \
                     "&" + _QUERY_PAGE_START_TOKEN + "=" + str(self._current) + \
                     "&" + _QUERY_PAGE_LENGTH_TOKEN + "=" + str(_DEFAULT_SAMPLES_PER_PAGE)
 
         self._current += 1
+
+        print(final_uri)
 
         r = requests.get(final_uri)
         if r.status_code == 200:
@@ -194,17 +116,15 @@ class Client(object):
                 # todo raise error
                 pass
             else:
-                url_base = _DEFAULT_BASE_URL + \
-                           self._parse_search(date_ini, date_end)
+                url_base = self._parse_search(date_ini, date_end)
 
                 if index_type == "monitors":
-                    url_base = url_base  + self._parse_monitor(query[index_type][index])
+                    url_base = url_base + self._parse_monitor(query[index_type][index])
                 elif index_type == "magnitudes":
-                    url_base = url_base  + self._parse_magnitude(query[index_type][index])
+                    url_base = url_base + self._parse_magnitude(query[index_type][index])
 
         else:
-            url_base = _DEFAULT_BASE_URL + \
-                       self._parse_search(date_ini, date_end) + \
+            url_base = self._parse_search(date_ini, date_end) + \
                        self._parse_monitors(query['monitors']) + \
                        self._parse_magnitudes(query['magnitudes'])
 
@@ -232,6 +152,8 @@ if __name__ == "__main__":
 
     client = Client()
     client.get("MACS/AzimuthAxis","position")
+
+
     #cursor = client.execute(query['search']['date_ini'],
     #                        query['search']['date_end'],
     #                        query, index=0, index_type="monitors")
