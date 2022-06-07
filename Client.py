b1b401b26e9bee7de86817914f7e57210521abbb
mhuertas <manuel.huertas.lopez@gmail.com>
2022-06-07 05:22:43 +0000

    Refactoring to improve quality of code

--- Client.py
+++ Client.py
@@ -1,10 +1,9 @@
 from datetime import datetime, time
 
+import logging
 import requests
 import re
 import math
-
-from numpy import finfo
 
 _DEFAULT_BASE_URL = "http://calp-vwebrepo:8081/WebReport/rest/webreport"
 _DEFAULT_SAMPLES_PER_PAGE = 30000
@@ -14,12 +13,13 @@ _QUERY_MAGNITUDE_TOKEN = "idmagnitud"
 _QUERY_PAGE_START_TOKEN = "iDisplayStart"
 _QUERY_PAGE_LENGTH_TOKEN = "iDisplayLength"
 
+
 def _parse_raw_test(text):
     text = text.split('\n')
 
     text_header = ','.join(text[0].replace("/", ".").split(",")[1:])
-    #todo extract this info to metadata monitor
-    text_header = re.sub("\(.*\)", "", text_header)
+    # todo extract this info to metadata monitor
+    text_header = re.sub(r"\(.*\)", "", text_header)
 
     text_body = [','.join(line.split(",")[1:]) for line in text[1:]]
     text_body = '\n'.join(text_body)
@@ -27,30 +27,13 @@ def _parse_raw_test(text):
     return text_header, text_body
 
 
-class Cursor(object):
+class Executor(object):
 
-    def __init__(self, url_base, pages):
+    def __init__(self, uri):
+        self._uri = uri
 
-        self.url_base = url_base
-        self._current = 0
-        self._pages = pages
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self._current >= self._pages:
-            raise StopIteration
-
-        final_uri = _DEFAULT_BASE_URL + "/download" + self.url_base +  \
-                    "&" + _QUERY_PAGE_START_TOKEN + "=" + str(self._current) + \
-                    "&" + _QUERY_PAGE_LENGTH_TOKEN + "=" + str(_DEFAULT_SAMPLES_PER_PAGE)
-
-        self._current += 1
-
-        print(final_uri)
-
-        r = requests.get(final_uri)
+    def run(self):
+        r = requests.get(self._uri)
         if r.status_code == 200:
             header, body = _parse_raw_test(r.text)
         else:
@@ -60,83 +43,106 @@ class Cursor(object):
         return header + '\n' + body
 
 
+class Cursor(object):
+
+    def __init__(self, url_base, pages, initial_page=0):
+
+        self.url_base = url_base
+        self._current = initial_page
+        self._pages = pages
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._current >= self._pages:
+            raise StopIteration
+
+        final_uri = _DEFAULT_BASE_URL + "/download" + self.url_base + \
+                    "&" + _QUERY_PAGE_START_TOKEN + "=" + str(self._current) + \
+                    "&" + _QUERY_PAGE_LENGTH_TOKEN + "=" + str(_DEFAULT_SAMPLES_PER_PAGE)
+
+        self._uri = final_uri
+        logging.debug('Url %s ', final_uri)
+
+        self._current += 1
+
+        return Executor(final_uri)
+
+    def run(self):
+        response = requests.get(self._uri)
+        if response.status_code == 200:
+            header, body = _parse_raw_test(response.text)
+        else:
+            # todo check this
+            raise requests.exceptions.HTTPError
+
+        return self._current, header + '\n' + body
+
+
+def _parse_search(query_date_ini, query_date_end):
+
+    search_uri_part = ""
+    search_uri_part = search_uri_part + query_date_ini.strftime("/%d/%m/%Y@%H:%M:%S.%f")[:-3]
+    search_uri_part = search_uri_part + query_date_end.strftime("/%d/%m/%Y@%H:%M:%S.%f")[:-3]
+    search_uri_part = search_uri_part + "/0?"
+
+    return search_uri_part
+
+
+def _parse_single_monitor(a_query):
+    query_parsed = ""
+    if a_query["type"] == "monitors":
+        query_parsed = _QUERY_MONITOR_TOKEN
+    elif a_query["type"] == "magnitudes":
+        query_parsed = _QUERY_MAGNITUDE_TOKEN
+
+    query_parsed = query_parsed + "=" + str(a_query["id"])
+
+    return query_parsed
+
+
+def _parse_monitors(a_query):
+    query_monitor_uri = ""
+
+    if isinstance(a_query, (list, tuple)):
+        for q in a_query:
+            query_monitor_uri = query_monitor_uri + _parse_single_monitor(q) + "&"
+        query_monitor_uri = query_monitor_uri[:-1]
+    else:
+        query_monitor_uri = _parse_single_monitor(a_query)
+
+    return query_monitor_uri
+
+
+def _number_pages(query_date_ini, query_date_end, period):
+    return math.ceil((query_date_end - query_date_ini).total_seconds() *
+                     (1.0 / (period / 1000000.0)) / _DEFAULT_SAMPLES_PER_PAGE)
+
+
+def execute(query_date_ini, query_date_end, a_query, initial_page=0):
+    logging.debug('Execute query from %s to %s ', query_date_ini, query_date_end)
+
+    url_base = _parse_search(query_date_ini, query_date_end)
+
+    url_base = url_base + _parse_monitors(a_query)
+
+    # todo how to calculate page when there are multiple monitors or when magnitude
+    pages = _number_pages(query_date_ini,
+                          query_date_end,
+                          a_query["period"])
+
+    logging.debug('Number of pages %s', pages)
+
+    a_cursor = Cursor(url_base, pages, initial_page)
+
+    return a_cursor
+
+
 class Client(object):
 
     def __init__(self, base_url=_DEFAULT_BASE_URL):
         self.base_url = base_url
-
-    def _parse_search(self, date_ini, date_end):
-
-        search_uri_part = ""
-        search_uri_part = search_uri_part + date_ini.strftime("/%d/%m/%Y@%H:%M:%S.%f")[:-3]
-        search_uri_part = search_uri_part + date_end.strftime("/%d/%m/%Y@%H:%M:%S.%f")[:-3]
-        search_uri_part = search_uri_part + "/0?"
-
-        return search_uri_part
-
-    def _parse_monitor(self, monitor):
-
-        return _QUERY_MONITOR_TOKEN + "=" + str(monitor["id"])
-
-    def _parse_magnitude(self, magnitude):
-
-        return _QUERY_MAGNITUDE_TOKEN + "=" + str(magnitude["id"])
-
-    def _parse_monitors(self, monitors):
-
-        query_monitor_uri_part = ""
-
-        for m in monitors:
-            query_monitor_uri_part = query_monitor_uri_part + self._parse_monitor(m) + "&"
-
-        query_monitor_uri_part = query_monitor_uri_part[:-1]
-
-        return query_monitor_uri_part
-
-    def _parse_magnitudes(self, magnitudes):
-
-        query_magnitude_uri_part = ""
-
-        for m in magnitudes:
-            query_magnitude_uri_part = query_magnitude_uri_part + self._parse_magnitude(m) + "&"
-
-        query_magnitude_uri_part = query_magnitude_uri_part[:-1]
-
-        return query_magnitude_uri_part
-
-    def _number_pages(self, date_ini, date_end, period):
-
-        return math.ceil((date_end - date_ini).seconds *
-                         (1.0 / (period / 1000000.0)) / _DEFAULT_SAMPLES_PER_PAGE)
-
-    def execute(self, date_ini, date_end, query, index=None, index_type=None):
-
-        if index is not None:
-            if index_type is None:
-                # todo raise error
-                pass
-            else:
-                url_base = self._parse_search(date_ini, date_end)
-
-                if index_type == "monitors":
-                    url_base = url_base + self._parse_monitor(query[index_type][index])
-                elif index_type == "magnitudes":
-                    url_base = url_base + self._parse_magnitude(query[index_type][index])
-
-        else:
-            url_base = self._parse_search(date_ini, date_end) + \
-                       self._parse_monitors(query['monitors']) + \
-                       self._parse_magnitudes(query['magnitudes'])
-
-
-        # todo the maximum period
-        pages = self._number_pages(date_ini,
-                                   date_end,
-                                   query['monitors'][0]['period'])
-
-        cursor = Cursor(url_base, pages)
-
-        return cursor
 
     def get(self, component, monitor):
 
@@ -146,18 +152,57 @@ class Client(object):
 
         print(final_uri)
 
+#todo move this to unit testing
+
+query = {
+
+    "search":
+        {
+            "date_ini": "2021-03-01",
+            "date_end": "2021-03-02",
+            "time_ini": "00:00:00",
+            "time_end": "00:00:00"
+        },
+
+    "monitors":
+        [
+            {
+                "id": 3623,
+                "name": "MACS.AzimuthAxis.position",
+                "period": 200000,
+                "epsilon": 0.5,
+                "type": "monitors"
+            }
+        ]
+}
 
 if __name__ == "__main__":
 
+    logging.basicConfig(level=logging.DEBUG)
+
+    date_ini = datetime.strptime(query['search']['date_ini'] +
+                                 " " +
+                                 query['search']['time_ini'],
+                                 '%Y-%m-%d %H:%M:%S')
+
+    date_end = datetime.strptime(query['search']['date_end'] +
+                                 " " +
+                                 query['search']['time_end'],
+                                 '%Y-%m-%d %H:%M:%S')
 
     client = Client()
-    client.get("MACS/AzimuthAxis","position")
+    cursor = execute(date_ini,
+                            date_end,
+                            query['monitors'][0])
+    for r in cursor:
+        print(r.run())
 
+    # client.get("MACS/AzimuthAxis","position")
 
-    #cursor = client.execute(query['search']['date_ini'],
+    # cursor = client.execute(query['search']['date_ini'],
     #                        query['search']['date_end'],
     #                        query, index=0, index_type="monitors")
-    #for r in cursor:
+    # for r in cursor:
     #    print(r)
 
     # print(client._parse_search(query['search']))
--- data_reduction_pipeline.py
+++ data_reduction_pipeline.py
@@ -1,67 +1,56 @@
 import logging
 
-from Client import Client
+from Client import Client, execute
 
-from datetime import timedelta, datetime, time
+from datetime import timedelta, datetime
 import pandas as pd
 import io
 import os
-import re
 from os.path import exists
 import glob
 import argparse
 import json
 
+
+def remove_similar_consecutive_values(data_frame, monitor_name, epsilon):
+
+    to_remove = []
+
+    pivot = data_frame[monitor_name][0]
+    for idx, row in data_frame.iterrows():
+        if idx == 0:
+            continue
+        if abs(pivot - row[monitor_name]) < epsilon:
+            to_remove.append(idx)
+        else:
+            pivot = row[monitor_name]
+
+    data_frame.drop(to_remove, axis=0, inplace=True)
+
+    return data_frame
+
+
 class DataReductionPipeline(object):
 
-    def __init__(self, query, name):
+    def __init__(self, a_query, name):
         self._client = Client()
 
         self._path = os.path.expanduser("~") + "/.cache/webreport/" + name + "/"
-        self._query_data = query
+        self._query_data = a_query
 
-    def make_file_name(self, date_ini, date_end, id,page=None):
+        self._date_ini = datetime.strptime(self._query_data['search']['date_ini'], '%Y-%m-%d')
+        self._date_end = datetime.strptime(self._query_data['search']['date_end'], '%Y-%m-%d')
 
-        if not os.path.exists(self._path):
-            os.makedirs(self._path)
-
-        file_name_monitor = str(id) + \
-                            "_" + \
-                            date_ini.strftime("%Y_%m_%d_%H_%M_%S") + \
-                            "_" + \
-                            date_end.strftime("%Y_%m_%d_%H_%M_%S")
-
-        if page is None:
-            file_name = self._path + file_name_monitor + ".gz"
-        else:
-            file_name = self._path + file_name_monitor + "_raw_" + str(page) + ".gz"
-
-        return file_name
-
-    def filter(self,data_frame,monitor_name,epsilon):
-
-        to_remove = []
-
-        pivot = data_frame[monitor_name][0]
-        for idx, row in data_frame.iterrows():
-            if idx == 0:
-                continue
-            if abs(pivot - row[monitor_name]) < epsilon:
-                to_remove.append(idx)
-            else:
-                pivot = row[monitor_name]
-
-        data_frame.drop(to_remove, axis=0, inplace=True)
-
-        return data_frame
+        self._time_ini = datetime.strptime(self._query_data['search']['time_ini'], '%H:%M:%S')
+        self._time_end = datetime.strptime(self._query_data['search']['time_end'], '%H:%M:%S')
 
     def merge_all_samples(self):
 
         logging.info('Merging all files...')
 
-        logging.info(self._path + "*.gz")
-        file_names = glob.glob(self._path + "*.gz")
-        file_names = [fn for fn in file_names if not "raw" in fn]
+        search = self._path + "**/*.gz"
+        file_names = glob.glob(search, recursive=True)
+        file_names = [fn for fn in file_names if "raw" not in fn]
 
         data_frames = [pd.read_csv(filename) for filename in file_names]
 
@@ -71,118 +60,96 @@ class DataReductionPipeline(object):
             for idx in range(2, len(data_frames)):
                 data_frame = pd.merge(data_frame, data_frames[idx], how='outer')
 
+        data_frame['TimeStampLong'] = pd.to_datetime(data_frame['TimeStampLong'], unit='us')
         data_frame.sort_values(by=['TimeStampLong'], inplace=True)
-        data_frame.to_csv(self._path + "merge_samples.gz", compression='infer',index=False)
+        data_frame.to_csv(self._path + "merge_samples.gz", compression='infer', index=False)
 
         return data_frame
 
-    def retrieve_all_samples(self):
+    def make_raw_file_name(self, date_ini, date_end, a_id, page=None):
 
-        for idx, m in enumerate(self._query_data['monitors']):
+        path = self._path + "/" + date_ini.strftime("%Y-%m-%d") + "/" + str(a_id).replace(".", "/") + "/raw/"
+        if not os.path.exists(path):
+            os.makedirs(path)
 
-            date_ini = datetime.strptime(self._query_data['search']['date_ini'] +
-                                         " " +
-                                         self._query_data['search']['time_ini'],
-                                         '%Y-%m-%d %H:%M:%S')
+        file_name_monitor = str(a_id) + \
+                            "." + \
+                            date_ini.strftime("%Y-%m-%d_%H_%M_%S") + \
+                            "." + \
+                            date_end.strftime("%Y-%m-%d_%H_%M_%S")
+        if page is None:
+            file_name_monitor = file_name_monitor + ".gz"
+        else:
+            file_name_monitor = file_name_monitor + ".raw." + str(page).rjust(4, '0') + ".gz"
 
-            date_end = datetime.strptime(self._query_data['search']['date_end'] +
-                                         " " +
-                                         self._query_data['search']['time_end'],
-                                         '%Y-%m-%d %H:%M:%S')
+        return path + "/" + file_name_monitor
 
-            file_name = self.make_file_name(date_ini,
-                                            date_end,
-                                            m['name'])
+    def make_filter_file_name(self, date_ini, date_end, a_id):
+        path = self._path + "/" + date_ini.strftime("%Y-%m-%d") + "/" + str(a_id).replace(".", "/")
 
-            logging.info('Retrieve all samples of monitor %s safe to file %s ...', m['name'], file_name)
+        if not os.path.exists(path):
+            os.makedirs(path)
 
-            if not exists(file_name):
+        file_name_monitor = str(a_id) + \
+                            "." + \
+                            date_ini.strftime("%Y-%m-%d_%H_%M_%S") + \
+                            "." + \
+                            date_end.strftime("%Y-%m-%d_%H_%M_%S")
+        file_name_monitor = file_name_monitor + ".gz"
 
-                data_frames_days = []
-                for n in range(0, (date_end-date_ini).days+1):
-                    logging.info('Retrieve samples of monitor %s day %s ...', m['name'], n)
+        return path + "/" + file_name_monitor
 
-                    cursor = self._client.execute(date_ini, date_end, query, index=idx, index_type="monitors")
+    def func(self, date_ini, date_end, monitor):
 
-                    data_frames_page = []
-                    try:
-                        for p, r in enumerate(cursor):
-                            logging.info('Retrieve samples of monitor %s day %s page %s...', m['name'], n, p)
+        data_frames_page = []
+        cursor = execute(date_ini, date_end, monitor)
 
-                            data_frame = pd.read_csv(io.StringIO(r), sep=",")
+        try:
+            for p, r in enumerate(cursor):
+                file_name = self.make_raw_file_name(date_ini, date_end, monitor['name'], p)
+                if not exists(file_name):
+                    logging.info('Retrieve samples of monitor page %s...', p)
+                    data_frame = pd.read_csv(io.StringIO(r.run()), sep=",")
+                    data_frame.to_csv(file_name, index=False, compression='infer')
+                else:
+                    data_frame = pd.read_csv(file_name)
+                data_frames_page.append(data_frame)
+        except:
+            print("An exception occurred")
 
-                            raw_file_name = self.make_file_name(date_ini, date_end, m['name'], p)
-                            if not exists(raw_file_name):
-                                data_frame.to_csv(raw_file_name, index=False, compression='infer')
+        data_frame = pd.concat(data_frames_page, ignore_index=True, sort=False)
 
-                            data_frames_page.append(data_frame)
+        return data_frame
 
-                    except:
-                        print("An exception occurred")
+    def retrieve_all_samples(self, func):
 
-                    data_frame = pd.concat(data_frames_page, ignore_index=True, sort=False)
-                    data_frame = self.filter(data_frame, m["name"], m["epsilon"])
+        date_range_ini = datetime.combine(self._date_ini, self._time_ini.time())
+        # todo be carefull when the time range is in the same day, not plus 1 day.
+        date_range_end = datetime.combine(self._date_ini, self._time_end.time()) + timedelta(days=1)
 
-                    data_frames_days.append(data_frame)
+        while date_range_ini < self._date_end:
 
-                    date_ini = date_ini + timedelta(days=1)
-                    date_end = date_end + timedelta(days=1)
+            for idx, m in enumerate(self._query_data['monitors']):
+                logging.info('Retrieve all samples of monitor %s %s from %s to %s...',
+                             idx, m['name'], date_range_ini, date_range_end)
 
-                data_frame = pd.concat(data_frames_days, ignore_index=True, sort=False)
-                data_frame['TimeStampLong'] = pd.to_datetime(data_frame['TimeStampLong'], unit='us')
+                data_frame = func(date_range_ini, date_range_end, m)
 
-                data_frame.to_csv(file_name, index=False, compression='infer')
+                # todo filter for magnitude
+                file_name = self.make_filter_file_name(date_range_ini, date_range_end, m['name'])
+                if not exists(file_name):
+                    if m["type"] == "monitors":
+                        data_frame = remove_similar_consecutive_values(data_frame, m["name"], m["epsilon"])
+                        data_frame.to_csv(file_name, index=False, compression='infer')
+                    else:  # todo coping the same file for simple merge, review this.
+                        data_frame.to_csv(file_name, index=False, compression='infer')
 
-    def retrieve_magnitude(self):
-
-        for idx, m in enumerate(self._query_data['magnitudes']):
-
-            date_ini = datetime.strptime(self._query_data['search']['date_ini'] +
-                                         " " +
-                                         self._query_data['search']['time_ini'],
-                                         '%Y-%m-%d %H:%M:%S')
-
-            date_end = datetime.strptime(self._query_data['search']['date_end'] +
-                                         " " +
-                                         self._query_data['search']['time_end'],
-                                         '%Y-%m-%d %H:%M:%S')
-
-            file_name = self.make_file_name(date_ini,
-                                            date_end,
-                                            m['name'])
-
-            if not exists(file_name):
-
-                data_frames_days = []
-                for n in range(0, (date_end-date_ini).days+1):
-
-                    cursor = self._client.execute(date_ini, date_end, query, index=idx, index_type="magnitudes")
-                    data_frames_page = []
-                    try:
-                        for r in cursor:
-                            data_frame = pd.read_csv(io.StringIO(r), sep=",")
-                            data_frames_page.append(data_frame)
-
-                    except:
-                        print("An exception occurred")
-
-                    data_frame = pd.concat(data_frames_page, ignore_index=True, sort=False)
-
-                    data_frames_days.append(data_frame)
-
-                    date_ini = date_ini + timedelta(days=1)
-                    date_end = date_end + timedelta(days=1)
-
-                data_frame = pd.concat(data_frames_days, ignore_index=True, sort=False)
-                data_frame['TimeStampLong'] = pd.to_datetime(data_frame['TimeStampLong'], unit='us')
-
-                print(file_name)
-                data_frame.to_csv(file_name, index=False, compression='infer')
+            date_range_ini = date_range_ini + timedelta(days=1)
+            date_range_end = date_range_end + timedelta(days=1)
 
 
 if __name__ == "__main__":
-
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
     logging.info('Data Reduction Pipeline Start Up!')
 
     parser = argparse.ArgumentParser(description='Monitor Manager Web Report Api.')
@@ -194,7 +161,5 @@ if __name__ == "__main__":
         query = json.load(json_file)
 
     pipeline = DataReductionPipeline(query, args.name)
-    pipeline.retrieve_all_samples()
-    pipeline.retrieve_magnitude()
+    pipeline.retrieve_all_samples(pipeline.func)
     pipeline.merge_all_samples()
-
